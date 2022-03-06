62423fe5c76c4136a91a98d0404dafc59f795880
fanakime <guilhem.romann@hotmail.fr>
2022-03-06 23:58:28 +0100

    a

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class main 
+{
+    public static void main(String[] args)
+    {
+        List<String> ficheAgent ;
+        List<String> staffs = new ArrayList<>();
+        Path pathAgent = Path.get("/text");
+        Path pathequi = Path.get("/text");
+    }
+    
+
+}
