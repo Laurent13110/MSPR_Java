a5b0cafb83a85072bfd4743972979b72a4a8cb4a
fanakime <guilhem.romann@hotmail.fr>
2022-03-07 18:36:09 +0100

    up java

--- main.java
+++ main.java
@@ -7,18 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.*;
+import java.nio.file.*;
+
 
 public class main 
 {
     /**
     * @param args
+     * @throws IOException
     */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         List<String> ficheAgent ;
         List<String> staffs = new ArrayList<>();
-        Path pathAgent = Path.get("/text");
-        Path pathequi = Path.get("/text");
+      //  Path chemin = Paths.get("intro.txt");
+        File doc = new File("\\text\\list.txt");
+
+        BufferedReader obj = new BufferedReader(new FileReader(doc));
+
+        String strng;
+        while ((strng = obj.readLine()) != null)
+          System.out.println(strng);
     }
     
 
