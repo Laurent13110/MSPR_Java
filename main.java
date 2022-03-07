47ea3acfe1ac070dab3ee4c89f58cc152ceb16c5
fanakime <guilhem.romann@hotmail.fr>
2022-03-07 20:38:56 +0100

    up java

--- main.java
+++ main.java
@@ -19,17 +19,33 @@ public class main
     */
     public static void main(String[] args) throws IOException
     {
-        List<String> ficheAgent ;
+        List<String> ficheAgent;
         List<String> staffs = new ArrayList<>();
       //  Path chemin = Paths.get("intro.txt");
-        File doc = new File("\\text\\list.txt");
+        File doc = new File("text/liste.txt");
 
         BufferedReader obj = new BufferedReader(new FileReader(doc));
 
         String strng;
         while ((strng = obj.readLine()) != null)
-          System.out.println(strng);
+        System.out.println(strng);
     }
-    
+
+  public static String RecupItem(String typeEquip) throws IOException 
+  {
+  // Création d’un fileReader pour lire le fichier
+  FileReader fileReader = new FileReader("text/liste.txt");
+
+  // Création d’un bufferedReader qui utilise le fileReader 
+  BufferedReader obj = new BufferedReader (fileReader);
+
+    String strng =strng = obj.readLine();  
+    while (strng == typeEquip)
+    {
+      System.out.println(strng);
+      strng = obj.readLine();
+    }
+    return strng;
+  }
 
 }
